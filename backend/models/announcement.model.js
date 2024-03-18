3fae42c97bd096dd02295c21a4efdb82c1643c54
Trushang-Patel <22it116@charusat.edu.in>
2024-03-18 21:01:47 +0530

    Frontend done

--- backend/models/announcement.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const announcementSchema = new Schema({
-  classCode: { type: String, required: true },
-  announcements: { type: Array },
-});
-
-module.exports = mongoose.model("Announcement", announcementSchema);
--- backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { signIn, login } = require("../controllers/user");
-
-router.post("/signin", signIn);
-router.post("/login", login);
-
-module.exports = router;
