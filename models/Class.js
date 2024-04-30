73a99e714cae7c2d1d86c88c8c1dd4675ec27495
Trushang-Patel <22it116@charusat.edu.in>
2024-04-30 21:33:29 +0530

    Final commit

---
+++ models/Class.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const ClassSchema = mongoose.Schema(
+    {
+        title: {
+            //  Title of the class
+            type: String,
+            require: true,
+        },
+        subjectCode: { 
+            type: String,
+            unique: true, // This ensures that each document has a unique subjectCode
+            sparse: true, // This allows multiple documents without a subjectCode field
+        },
+        books: [{ type: String }], //  Recommended books by admin
+        link: {
+            //  link to the video room
+            type: String,
+        },
+        admin: {
+            //  Admin of the class or teacher
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                require: true,
+            },
+            name: {
+                type: String,
+                require: true,
+            },
+        },
+        // posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }], // All the posts of the class
+        students: [
+            {
+                user: { type: String, ref: 'User' },
+                points: { type: Number, default: 0 },
+            },
+        ], //  Students in the class // referencing emails here
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Class = mongoose.model('Class', ClassSchema);
+
+module.exports = { Class, ClassSchema };
